7e13875306b481c78548886869f69b246d9afa91
Amal <amalessid98@gmail.com>
2023-09-15 11:16:30 +0100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export const Header = () => {
+  return (
+    <div >
+      <br />
+      <br />
+      <h1 className = "text-center text-5xl talic font-custom text-indigo-800">
+        TODO LIST
+      </h1>
+    </div>
+  );
+};
